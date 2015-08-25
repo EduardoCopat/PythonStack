28824e5a162d699143e1cb2878d0bda972a652a8
EduardoCopat <ecopat@gmail.com>
2015-08-24 22:02:43 -0300

    Second + third

--- Stack.py
+++ Stack.py
@@ -1,1 +1,16 @@
 __author__ = 'Eduardo'
+
+class Stack(object):
+
+    def __init__(self):
+        self.stack_size = 0
+
+    def push(self, element):
+        self.stack_size += 1
+
+    def pop(self):
+        self.stack_size -= 1
+        return "b"
+
+    def size(self):
+        return self.stack_size
--- StackTest.py
+++ StackTest.py
@@ -1,1 +1,30 @@
 __author__ = 'Eduardo'
+
+import unittest
+from Stack import Stack
+
+
+
+class TestStack(unittest.TestCase):
+
+    def setUp(self):
+        self.stack = Stack()
+
+    def test_push_element_size_1(self):
+        self.stack.push("a")
+        self.assertEqual(self.stack.size(), 1)
+
+    def test_two_pushes_size_2(self):
+        self.stack.push("a")
+        self.stack.push("b")
+        self.assertEqual(self.stack.size(), 2)
+
+    def test_two_pushes_one_pop(self):
+        self.stack.push("a")
+        self.stack.push("b")
+
+        self.assertEqual(self.stack.pop(), "b")
+        self.assertEqual(self.stack.size(), 1)
+
+
+
